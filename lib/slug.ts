f5f46954d4d69c8d07803dc106b9c3e755f6898d
nadinesuhendro-png <nadinesuhendro@gmail.com>
2026-09-08 07:27:28 +0700

    Create slug.ts

---
+++ lib/slug.ts
@@ -0,0 +1,12 @@
+// AKSI: BUAT FILE BARU
+// PATH: lib/slug.ts
+
+export function slugify(input: string): string {
+  return input
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
+
